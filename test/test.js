f1cf208e34d344139c2a92f8949461c1cb3e8a57
Ganja Vasiliy <vasllly219@gmail.com>
2019-03-18 03:00:09 +0300

    FIX tets description

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
 import validator from '../src/js/script';
 
-test('fetchData return status error', () => {
+test('validator return 1', () => {
   const received = validator('1');
   const expected = 1;
   expect(received).toBe(expected);
 });
 
-test('fetchData return status error', () => {
+test('validator return type error', () => {
   function inputString() {
     validator('string');
   }
